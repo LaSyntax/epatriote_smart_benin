f5a24785efbb58815050eaf8567f33739dfb8ca8
Justin Dah-kenangnon <dah.kenangnon@gmail.com>
2021-05-19 19:03:48 +0100

    Add WidgetDisteet

---
+++ lib/src/disteet_widget.dart
@@ -0,0 +1,11 @@
+class DistreetWidget extends StatefulWidget {
+  @override
+  _DistreetWidgetState createState() => _DistreetWidgetState();
+}
+
+class _DistreetWidgetState extends State<DistreetWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
